b417a0cad9922081b8313812e48cdbf6b95cbe04
Hao Bryan Cheng <haobcheng@gmail.com>
2017-10-14 21:48:10 -0700

    Cleaning up a few broken helper interfaces

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -635,7 +635,7 @@ func (s *PublicBlockChainAPI) doCall(ctx context.Context, args CallArgs, blockNr
 	// Setup the gas pool (also for unmetered requests)
 	// and apply the message.
 	gp := new(core.GasPool).AddGas(math.MaxBig256)
-	res, gas, failed, err := core.ApplyMessage(evm, msg, gp)
+	res, gas, failed, _, err := core.ApplyMessage(evm, msg, gp)
 	if err := vmError(); err != nil {
 		return nil, common.Big0, false, err
 	}
